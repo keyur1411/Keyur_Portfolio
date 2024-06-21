66ee11883b520a26207a7e10765e586609cb7e54
keyur1411 <keyur14113@gmail.com>
2024-06-21 12:01:35 +0530

    Completed website As per Tutorial But will do changes after watching some more tutorial and little more learning abour CSS & Javascipt

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 var sidemeu = document.getElementById("sidemenu");
-    function openmenu(){
-        sidemeu.style.width="230px";
-    }
+function openmenu() {
+  sidemeu.style.width = "230px";
+}
 
-    function closemenu(){
-        sidemeu.style.width="0";
-    }
+function closemenu() {
+  sidemeu.style.width = "0";
+}
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxP_LLJf2PwYwKBWIbPoWLlVgXLN8aoJwQTlx6B8F1qs7SFRhLWPUY-ozCF9DkdixUa/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      msg.innerHTML ="Message Sent Successfully !"
+      setTimeout(function(){
+        msg.innerHTML=""
+      },5000)
+      form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
